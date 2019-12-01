906379b7e79e5a312ff36ad861ef33473c3193a5
limon15 <so.martignetti@gmail.com>
2019-12-01 18:05:00 -0300

    Incorporo puntos 3 y 4 y realizo pequeñas modificaciones en casos donde el cliente no se encuentre en un rango donde restaurantes envien pedidos

--- info_predefinida.py
+++ info_predefinida.py
@@ -23,12 +23,12 @@ def devolver_data_de_archivos_pickle(nombre_entidad, nombre_adicional=''):
 
 def info_predefinida():
     restaurantes = [
-    {'Nombre': 'MARIA BONITA', 'Direccion': 'Mitre 1195, Adrogue, Buenos Aires', 'Telefono': '011 4294-1184', 'Posicion': (-34.797054, -58.391627), 'Radio de entrega': 2.5, 'Platos': [{'Nombre': 'Ensalada Ninetta', 'Precio': 250},{'Nombre': 'Rissotto ai funghi', 'Precio': 370}, {'Nombre': 'Brotola a los 4 quesos', 'Precio': 570}], 'Total de ventas': 0, 'Moneda': 'ARG'},
-    {'Nombre': 'PASTA ROSSA', 'Direccion': 'Jorge San Pellerano 754, Adrogue, Buenos Aires', 'Telefono': '011 4214-3437', 'Posicion': (-34.799433, -58.390941), 'Radio de entrega': 1, 'Platos': [{'Nombre': 'Gnocchi souffle', 'Precio': 219},{'Nombre': 'Ravioli di formaggio', 'Precio': 229}, {'Nombre': 'Sorrentino di salmone', 'Precio': 310}], 'Total de ventas': 0, 'Moneda': 'ARG'},
-    {'Nombre': 'PIZZERIA EL FARO', 'Direccion': 'Esteban Adrogué 1187, Adrogue, Buenos Aires', 'Telefono': '011 4214-4144', 'Posicion': (-34.798170, -58.390783), 'Radio de entrega': 3.5, 'Platos': [{'Nombre': 'Pizza muzzarella', 'Precio': 150},{'Nombre': 'Pizza napolitana', 'Precio': 170}, {'Nombre': 'Pizza ananá con azucar', 'Precio': 200}], 'Total de ventas': 0, 'Moneda': 'ARG'},
-    {'Nombre': 'TIRIFILO EL BODEGON', 'Direccion': 'Cordero 694, Adrogue, Buenos Aires', 'Telefono': '011 4294-4195', 'Posicion': (-34.801080, -58.395488), 'Radio de entrega': 5, 'Platos': [{'Nombre': 'Milanesa con ensalada', 'Precio': 200},{'Nombre': 'Salteado de carne', 'Precio': 250}, {'Nombre': 'Guiso de mondongo', 'Precio': 320}], 'Total de ventas': 0, 'Moneda': 'ARG'},
-    {'Nombre': 'SUSHI ADROGUE', 'Direccion': 'Int. Dr. Martín González 806, Adrogue, Buenos Aires', 'Telefono': '0810-220-2006', 'Posicion': (-34.798375, -58.396260), 'Radio de entrega': 0.5, 'Platos': [{'Nombre': 'Uramaki', 'Precio': 300},{'Nombre': 'Nirigi de atun', 'Precio': 350}, {'Nombre': 'Dorayakis', 'Precio': 400}], 'Total de ventas': 0, 'Moneda': 'ARG'}
-    ]
+        {'Nombre': 'MARIA BONITA', 'Direccion': 'Mitre 1195, Adrogue, Buenos Aires', 'Telefono': '011 4294-1184', 'Posicion': (-34.797054, -58.391627), 'Radio de entrega': 2.5, 'Platos': [{'Nombre': 'Ensalada Ninetta', 'Precio': 250},{'Nombre': 'Rissotto ai funghi', 'Precio': 370}, {'Nombre': 'Brotola a los 4 quesos', 'Precio': 570}], 'Total de ventas': 0, 'Moneda': 'ARG'},
+        {'Nombre': 'PASTA ROSSA', 'Direccion': 'Jorge San Pellerano 754, Adrogue, Buenos Aires', 'Telefono': '011 4214-3437', 'Posicion': (-34.799433, -58.390941), 'Radio de entrega': 1, 'Platos': [{'Nombre': 'Gnocchi souffle', 'Precio': 219},{'Nombre': 'Ravioli di formaggio', 'Precio': 229}, {'Nombre': 'Sorrentino di salmone', 'Precio': 310}], 'Total de ventas': 0, 'Moneda': 'ARG'},
+        {'Nombre': 'PIZZERIA EL FARO', 'Direccion': 'Esteban Adrogué 1187, Adrogue, Buenos Aires', 'Telefono': '011 4214-4144', 'Posicion': (-34.798170, -58.390783), 'Radio de entrega': 3.5, 'Platos': [{'Nombre': 'Pizza muzzarella', 'Precio': 150},{'Nombre': 'Pizza napolitana', 'Precio': 170}, {'Nombre': 'Pizza ananá con azucar', 'Precio': 200}], 'Total de ventas': 0, 'Moneda': 'ARG'},
+        {'Nombre': 'TIRIFILO EL BODEGON', 'Direccion': 'Cordero 694, Adrogue, Buenos Aires', 'Telefono': '011 4294-4195', 'Posicion': (-34.801080, -58.395488), 'Radio de entrega': 5, 'Platos': [{'Nombre': 'Milanesa con ensalada', 'Precio': 200},{'Nombre': 'Salteado de carne', 'Precio': 250}, {'Nombre': 'Guiso de mondongo', 'Precio': 320}], 'Total de ventas': 0, 'Moneda': 'ARG'},
+        {'Nombre': 'SUSHI ADROGUE', 'Direccion': 'Int. Dr. Martín González 806, Adrogue, Buenos Aires', 'Telefono': '0810-220-2006', 'Posicion': (-34.798375, -58.396260), 'Radio de entrega': 0.5, 'Platos': [{'Nombre': 'Uramaki', 'Precio': 300},{'Nombre': 'Nirigi de atun', 'Precio': 350}, {'Nombre': 'Dorayakis', 'Precio': 400}], 'Total de ventas': 0, 'Moneda': 'ARG'}
+        ]
     clientes = [
         {'Nombre de usuario': 'LUCHIA31', 'Contraseña': 'Buonabitacolo31!', 'Telefono': '011 4214-7576', 'Direccion': 'Erezcano 1576, Adrogue, Buenos aires', 'Posicion': (-34.802668, -58.375369), 'Rappicreditos': 0},
         {'Nombre de usuario': '2019AMARIAG', 'Contraseña': 'nuncioYana2019?', 'Telefono': '011 4214-7576', 'Direccion': 'Av. Espora 200, Adrogué, Buenos aires', 'Posicion': (-34.788542, -58.389000), 'Rappicreditos': 0},
@@ -37,11 +37,11 @@ def info_predefinida():
         {'Nombre de usuario': 'FLEQUI26', 'Contraseña': 'lamunil*CA1', 'Telefono': '011 4294-3936', 'Direccion': 'Cerretti 876, Adrogue, Buenos aires', 'Posicion': (-34.799099, -58.386906), 'Rappicreditos': 0}
     ]
     rappitenderos = [
-        {'Nombre': 'LUCIANA ARCOIRIS', 'Propina acumulada': 0, 'Posicion actual': (-34.789540, -58.373989), 'Pedido actual': None},
-        {'Nombre': 'JONATHAN MOREL', 'Propina acumulada': 0, 'Posicion actual': (-34.797054, -58.391627), 'Pedido actual': None},
-        {'Nombre': 'AURELIANO OLIVA', 'Propina acumulada': 0, 'Posicion actual': (-34.799099, -58.386906), 'Pedido actual': None},
-        {'Nombre': 'ATENEA ANTI', 'Propina acumulada': 0, 'Posicion actual': (-34.788542, -58.389000), 'Pedido actual': None},
-        {'Nombre': 'MORFI MOCHUELO', 'Propina acumulada': 0, 'Posicion actual': (-34.802668, -58.375369), 'Pedido actual': None}
+        {'Nombre': 'LUCIANA ARCOIRIS', 'Propina acumulada': 0, 'Posicion actual': (-34.789540, -58.373989), 'Pedido actual': None, 'Distancia recorrida': 0},
+        {'Nombre': 'JONATHAN MOREL', 'Propina acumulada': 0, 'Posicion actual': (-34.797054, -58.391627), 'Pedido actual': None, 'Distancia recorrida': 0},
+        {'Nombre': 'AURELIANO OLIVA', 'Propina acumulada': 0, 'Posicion actual': (-34.799099, -58.386906), 'Pedido actual': None, 'Distancia recorrida': 0},
+        {'Nombre': 'ATENEA ANTI', 'Propina acumulada': 0, 'Posicion actual': (-34.788542, -58.389000), 'Pedido actual': None, 'Distancia recorrida': 0},
+        {'Nombre': 'MORFI MOCHUELO', 'Propina acumulada': 0, 'Posicion actual': (-34.802668, -58.375369), 'Pedido actual': None, 'Distancia recorrida': 0}
     ]
     generar_archivos_pickle(clientes, 'clientes', DATA_PRED)
     generar_archivos_pickle(restaurantes, 'restaurantes', DATA_PRED)
--- pedidos.py
+++ pedidos.py
@@ -2,6 +2,55 @@ from common import validar_opcion_ingresada, obtener_lista_nombres_restaurantes,
 from validaciones import no_existe_en_lista, alertar_error
 from cargar_data import validar_nombre_eleccion_entidad, PrettyTable
 from simulaciones import obtener_valor_al_azar_de_lista_de_dic, actualizar_item_lista_entidad
+from math import radians, cos, sin, asin, sqrt
+
+def actualizar_distancia_recorrida_rappitendero(rappitendero, distancia):
+    rappitendero['Distancia recorrida']+= distancia
+    return rappitendero
+
+
+def calcular_distancia_terrestre(lon1, lat1, lon2, lat2):
+
+    # Convierte grados decimales a radianes. Para esto importamos la librería math.
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # Formula de Haversine
+    # Fuente: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    radio = 6371 # Radio de la tierra en km.
+    
+    return c * radio
+
+def devolver_entidad_cercana(entidad, lista_entidades, clave_entidad_1, clave_entidad_2, codigo):
+    PROCESAR_RESTAURANTE = 1
+    PROCESAR_RAPPITENDERO = 2
+    lista_entidad_cercana = []
+    lon1= entidad[clave_entidad_1][0]
+    lat1= entidad[clave_entidad_1][1]
+    distancia_minima = 1000 #variable exagerada pero que será reemplazada por el primero valor de distancia calculada para luego ir comparándose durante el bucle.
+
+    for i in range(len(lista_entidades)):
+
+        lon2 = lista_entidades[i][clave_entidad_2][0]
+        lat2 = lista_entidades[i][clave_entidad_2][1]
+
+        distancia_entre_entidades = calcular_distancia_terrestre(lon1, lat1, lon2, lat2)
+
+        if codigo == PROCESAR_RESTAURANTE:
+            if lista_entidades[i]['Radio de entrega'] > distancia_entre_entidades:
+                lista_entidad_cercana.append(lista_entidades[i])
+
+        elif codigo == PROCESAR_RAPPITENDERO:
+            if distancia_entre_entidades < distancia_minima:
+                distancia_minima = distancia_entre_entidades
+                lista_entidad_cercana = lista_entidades[i]
+    
+    return lista_entidad_cercana
+
 
 # Función que muestra los platos enumerados, con su precio, del restaurante elegido.
 #Se muestra platos de la siguiente forma: 1,2...,n. Amentando en 1 la vista de la posición
@@ -21,14 +70,14 @@ def obtener_importe_pedido_manual(lista_platos, lista_pedidos):
     return importe_total
 
 def actualizar_ganancias_rappitendero_cliente(importe_pedido, rappitendero, cliente):
-    rappitendero['Propina acumulada'] += round(0.1*importe_pedido,2)
+    rappitendero['Propina acumulada'] += round(0.05*importe_pedido,2)
     if importe_pedido<200:
-        cliente['Rappicreditos'] += round(0.1*importe_pedido,2)
+        cliente['Rappicreditos'] += round(0.05*importe_pedido,2)
     elif 200<importe_pedido<500: 
-        cliente['Rappicreditos'] += round(0.15*importe_pedido,2)
+        cliente['Rappicreditos'] += round(0.10*importe_pedido,2)
     else: 
-        cliente['Rappicreditos'] += round(0.2*importe_pedido,2)
-    return rappitendero, cliente      
+        cliente['Rappicreditos'] += round(0.15*importe_pedido,2)
+    return rappitendero, cliente
 
 def actualizar_posicion_pedido_rappitendero(posicion_nueva, pedido, rappitendero):
     rappitendero['Posicion actual'] = posicion_nueva
@@ -136,26 +185,40 @@ def elegir_plato(restaurante):
     nombre_plato =  platos_restaurante[indice_plato]['Nombre']
     return nombre_plato
 
-def procesar_pedido_manual(pedido, cliente, restaurante, lista_clientes, lista_restaurantes, lista_rappitenderos):
+def procesar_pedido_manual(pedido, cliente, restaurante, lista_clientes, lista_restaurantes, lista_rappitenderos, lista_rappitendero_mas_cercano):
+
     print("\nEste es su pedido:\n")
-    generar_reporte_pedido(pedido)   
-    rappitendero_al_azar = obtener_valor_al_azar_de_lista_de_dic(lista_rappitenderos, "rappitendero", "Nombre")
+    generar_reporte_pedido(pedido)
+
+    posicion_rappitendero = lista_rappitendero_mas_cercano['Posicion actual']
     posicion_restaurante = restaurante['Posicion']
-    rappitendero_al_azar_actualizado = actualizar_posicion_pedido_rappitendero(posicion_restaurante, pedido, rappitendero_al_azar)
-    posicion_cliente = cliente['Posicion']  
+    posicion_cliente = cliente['Posicion'] 
+
+    distancia_rappitendero_restaurante = calcular_distancia_terrestre(posicion_rappitendero[0], posicion_rappitendero[1], posicion_restaurante[0], posicion_restaurante[1])
+
+    rappitendero_mas_cercano_actualizado = actualizar_distancia_recorrida_rappitendero(lista_rappitendero_mas_cercano, distancia_rappitendero_restaurante) ## Actualizo la distancia recorrida desde donde estaba el rappitendero hacia el restaurante.
+    rappitendero_mas_cercano_actualizado = actualizar_posicion_pedido_rappitendero(posicion_restaurante, pedido, rappitendero_mas_cercano_actualizado)
+ 
     print("\n => El pedido será entregado a la dirección: {}".format(cliente['Direccion']))      
-    distancia = calcular_distancia(rappitendero_al_azar_actualizado['Posicion actual'], cliente['Posicion'])
-    mostrar_tiempo_estimado(distancia)
-    rappitendero_al_azar_actualizado = actualizar_posicion_pedido_rappitendero(posicion_cliente, pedido, rappitendero_al_azar_actualizado)
-    platos_restaurante = restaurante['Platos']   
+
+    distancia_restaurante_cliente = calcular_distancia_terrestre(posicion_restaurante[0], posicion_restaurante[1], posicion_cliente[0], posicion_cliente[1])
+
+    mostrar_tiempo_estimado(distancia_restaurante_cliente)
+    rappitendero_mas_cercano_actualizado = actualizar_posicion_pedido_rappitendero(posicion_cliente, pedido, rappitendero_mas_cercano_actualizado)
+
+    platos_restaurante = restaurante['Platos']
     importe_total = obtener_importe_pedido_manual(platos_restaurante, pedido['Pedido'])
     print("\n => El importe total a pagar es de: ${}.".format(importe_total))
-    rappitendero_al_azar_actualizado, cliente_actualizado = actualizar_ganancias_rappitendero_cliente(importe_total, rappitendero_al_azar_actualizado, cliente)
+    rappitendero_mas_cercano_actualizado, cliente_actualizado = actualizar_ganancias_rappitendero_cliente(importe_total, rappitendero_mas_cercano_actualizado, cliente)
     print("\n => El pedido ya fue recibido en su domicilio. Usted ganó {} rappicreditos por la compra.".format(cliente_actualizado['Rappicreditos']))
+    
+    rappitendero_mas_cercano_actualizado = actualizar_distancia_recorrida_rappitendero(rappitendero_mas_cercano_actualizado, distancia_restaurante_cliente) ## Actualizo la distancia recorrida desde donde estaba el rappitendero hacia el cliente.
     restaurante_actualizado = actualizar_ventas_restaurante(importe_total, restaurante) 
+
     lista_clientes = actualizar_item_lista_entidad(lista_clientes, cliente, cliente_actualizado)
     lista_restaurantes = actualizar_item_lista_entidad(lista_restaurantes, restaurante, restaurante_actualizado)
-    lista_rappitenderos = actualizar_item_lista_entidad(lista_rappitenderos, rappitendero_al_azar, rappitendero_al_azar_actualizado)
+    lista_rappitenderos = actualizar_item_lista_entidad(lista_rappitenderos, lista_rappitendero_mas_cercano, rappitendero_mas_cercano_actualizado)
+
     print("\n Se cerrará su sesión.\n Hasta luego, vuelva pronto ☺.")
     return lista_clientes, lista_restaurantes, lista_rappitenderos
 
@@ -164,15 +227,24 @@ def pedido_manual(lista_clientes, lista_restaurantes, lista_rappitenderos):
     if existen_clientes:
         cliente = iniciar_sesion_cliente(lista_clientes)
         existen_restaurantes = evaluar_existencia_entidad(lista_restaurantes, 'restaurantes')
-        if existen_restaurantes:
+        
+        lista_restaurantes_cercanos = devolver_entidad_cercana(cliente, lista_restaurantes, 'Posicion', 'Posicion', 1) ## Agregué esta linea para obtener la lista de restaurantes cercanos.
+        
+        if (existen_restaurantes and len(lista_restaurantes_cercanos)!=0):
             continuar_pedido = True
-            pedido = { 'Pedido': [], 'Cliente': cliente }
-            restaurante = elegir_restaurante(lista_restaurantes)
+            pedido = { 'Pedido': [], 'Cliente': cliente['Nombre de usuario'] }
+            restaurante = elegir_restaurante(lista_restaurantes_cercanos)
+            
             while continuar_pedido:
                 nombre_plato = elegir_plato(restaurante)
                 cant_plato = solicitar_cantidad_platos(nombre_plato, 1, 10)
                 pedido['Pedido'] = actualizar_pedido(pedido['Pedido'], cant_plato, nombre_plato)
                 continuar_pedido = continuar_pidiendo()
-            lista_clientes, lista_restaurantes, lista_rappitenderos = procesar_pedido_manual(pedido, cliente, restaurante, lista_clientes, lista_restaurantes, lista_rappitenderos)     
+
+            lista_rappitendero_mas_cercano = devolver_entidad_cercana(restaurante, lista_rappitenderos, 'Posicion', 'Posicion actual', 2) ## Agregué esta linea para obtener la lista de datos del rappitendero mas cercano.
+            lista_clientes, lista_rappitendero_mas_cercano, lista_rappitenderos = procesar_pedido_manual(pedido, cliente, restaurante, lista_clientes, lista_restaurantes, lista_rappitenderos, lista_rappitendero_mas_cercano) ## Agregué un parámetro (el de último lugar).
+        else:
+            print("Lo sentimos. No existen restaurantes disponibles en tu zona.")    
+
     imprimir_aviso_de_retorno_al_menu_anterior()
-    return lista_clientes, lista_restaurantes, lista_rappitenderos 
+    return lista_clientes, lista_restaurantes, lista_rappitenderos    
